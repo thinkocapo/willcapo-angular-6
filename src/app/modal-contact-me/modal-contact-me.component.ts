17de5897adc6b92dbe0467c004c92af442fa8d6b
thinkocapo <thinkocapo@gmail.com>
2018-11-03 21:12:49 -0400

    modal opens

--- src/app/modal-contact-me/modal-contact-me.component.ts
+++ src/app/modal-contact-me/modal-contact-me.component.ts
@@ -7,7 +7,7 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./modal-contact-me.component.scss']
 })
 export class ModalContactMeComponent implements OnInit {
-  @ViewChild('modalContactMeReference') modalContactMeReference = ElementRef
+  @ViewChild('modalContactMeRef') modalContactMeRef = ElementRef
   closeResult: string;
 
   constructor(private modalService: NgbModal) {}
@@ -15,12 +15,15 @@ export class ModalContactMeComponent implements OnInit {
   ngOnInit() {
 
     console.log('* MODAL CONTACT ME *')
-    this.modalService.open(this.modalContactMeReference, {ariaLabelledBy: 'modal-basic-title', size: 'lg'}).result.then((result: any) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason: any) => {
-      console.log('reason', reason)
-      // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    setTimeout(() => {
+      this.modalService.open(this.modalContactMeRef, {ariaLabelledBy: 'modal-basic-title', size: 'lg'}).result.then((result: any) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason: any) => {
+        console.log('reason', reason)
+        // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      })
     })
+
   }
 
 }
