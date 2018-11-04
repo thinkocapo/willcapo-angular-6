e23629583df871d4ac9d269a2a5183d6adec6412
thinkocapo <thinkocapo@gmail.com>
2018-11-04 12:41:39 -0500

    tab icon updated

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { extract } from '@app/core';
 import { HomeComponent } from './home.component';
+import { ResumeComponent } from '../resume/resume.component';
 import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
   Shell.childRoutes([
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent, data: { title: extract('Home') } }
+    { path: 'home', component: HomeComponent, data: { title: extract('Home') } },
+    { path: 'resume', component: ResumeComponent, data: { title: extract('Resume') } }
   ])
 ];
 
