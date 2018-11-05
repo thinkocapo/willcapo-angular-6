e3047feda4c1335d31b254f65667bf57ff87eea0
thinkocapo <thinkocapo@gmail.com>
2018-11-04 19:50:04 -0500

    markdowntohtml

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,5 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+/*
+Markdown Editor but need Render Markdown
+http://inacho.github.io/bootstrap-markdown-editor/
+
+says use Jekyll to convert
+https://medium.freecodecamp.org/style-webpage-or-markdown-like-medium-article-using-html-css-sass-bootstrap-c6f9e64c0955
+
+
+STRAPDOWN  *
+http://strapdownjs.com/
+
+Markdown to HTML Online Converter **
+https://markdowntohtml.com/
+https://www.browserling.com/tools/markdown-to-html
+https://zapier.com/blog/markdown-html-export/
+*/
+
+
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
