4b4fd691577c4b32a1d11bdeb50ce6414bf189b5
William Capozzoli <thinkocapo@gmail.com>
2018-11-22 22:38:03 -0500

    All Front End Routes Added

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,13 +6,19 @@ import { HomeComponent } from './home.component';
 import { ResumeComponent } from '../resume/resume.component';
 import { Shell } from '@app/shell/shell.service';
 import { TravelComponent } from '@app/travel/travel.component';
+import { CodeComponent } from '@app/code/code.component';
+import { BlockchainComponent } from '@app/blockchain/blockchain.component';
+import { BooksComponent } from '@app/books/books.component';
+import { HealthcareComponent } from '@app/healthcare/healthcare.component';
 
 const routes: Routes = [
   Shell.childRoutes([
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'resume', component: ResumeComponent }, // data: { title: extract('Resume') } } // updates browser tab title
-    { path: 'travel', component: TravelComponent }
+    { path: 'travel', component: TravelComponent },
+    { path: 'code', component: CodeComponent }
+
   ])
 ];
 
